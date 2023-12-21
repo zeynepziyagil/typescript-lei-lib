0618e8a4942cd0ae7cbe3dd5aac999143f825d88
zeynep ziyagil <zeynep.ziyagil@derivasset.com>
2023-12-21 13:17:24 +0300

    new functions added

--- src/index.ts
+++ src/index.ts
@@ -59,3 +59,118 @@ export async function getFuzzyCompletions(field: filter, q: string): Promise<Fuz
 }
 
 
+export async function getLEIRecordsByISIN(isin: string): Promise<LEIRecord[]> {
+  const url = `${baseURL}lei-records?filter[isin]=${isin}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+
+export async function getLEIRecordsByBIC(bic: string): Promise<LEIRecord[]> {
+  const url = `${baseURL}lei-records?filter[bic]=${bic}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+
+export async function getLEIRecordsByLEI(leis: string[]): Promise<LEIRecord[]> {
+  const leiQueryString = leis.map((lei) => `filter[lei]=${lei}`).join(',');
+  const url = `${baseURL}lei-records?${leiQueryString}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+
+
+export async function getLEIRegistrationStatusByLEI(lei: string): Promise<string> {
+  const url = `${baseURL}lei-records/${lei}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  const registrationStatus = data.data?.attributes?.registration?.status;
+
+  if (!registrationStatus) {
+    throw new Error(`Unable to retrieve registration status for LEI: ${lei}`);
+  }
+
+  return registrationStatus;
+}
+
+export async function getLEIRecordsByStatus(status: string, pageNumber: number = 1, pageSize: number = 1): Promise<LEIRecord[]> {
+  const url = `${baseURL}lei-records?filter[registration.status]=${status}&page[number]=${pageNumber}&page[size]=${pageSize}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+export async function getLEIRecordsByCountry(countryCode: string, pageNumber: number = 1, pageSize: number = 1): Promise<LEIRecord[]> {
+  const url = `${baseURL}lei-records?filter[entity.legalAddress.country]=${countryCode}&page[number]=${pageNumber}&page[size]=${pageSize}`;
+
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      'Accept': 'application/vnd.api+json'
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status: ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.data;
+}
+
